8a547736c1333b9b42e535e3a14036e9bb085299
MhMd099 <moalalhussein@gmail.com>
2025-03-03 16:46:37 +0100

    ggggg

--- EgitTestMo/src/htl/HelloWorld.java
+++ EgitTestMo/src/htl/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
 		System.out.println("Hello, Mohammad");
-		System.out.println("Hello, Henry");
+		System.out.println("gHello, Henry");
+		System.out.println("Hello, github");
 	}
 }
